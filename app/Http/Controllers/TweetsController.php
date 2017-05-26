140863672803b0082c0e044b5dfe90a4ac780784
tech-camp <tech-camp@tech-camp-1-138-no-MacBook-Air.local>
2017-05-26 16:10:33 +0900

    initial commit

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+    //
+    public function index()
+    {
+        $tweets = Tweet::all();
+
+        return view('tweets.index')->with('tweets', $tweets);
+    }
+
+    public function create()
+    {
+        return view('tweets.create');
+    }
+
+    public function store(Request $request)
+    {
+        Tweet::create([
+          // 'image' => $request->image,
+          'text' => $request->text
+          // 'user_id' => Auth::user()->id
+          ]);
+        return redirect('tweets');
+    }
+}
